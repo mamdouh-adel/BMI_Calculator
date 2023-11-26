c207c94f2baaeead6627618bf94d8706e8803794
Mamdouh <mamdouh.elmallah@yahoo.com>
2023-11-26 21:39:29 +0200

    add Form Validation

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -7,6 +7,7 @@ import '../../shared/components/components.dart';
 class LoginScreen extends StatelessWidget {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   LoginScreen({super.key});
 
@@ -22,103 +23,110 @@ class LoginScreen extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Login",
-                  style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.email),
-                    labelText: ("Email Address"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.lock),
-                    suffixIcon: const Icon(Icons.remove_red_eye),
-                    labelText: ("Password"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    borderRadius:
-                        BorderRadiusDirectional.all(Radius.circular(20)),
-                    color: Colors.blue,
-                  ),
-                  child: MaterialButton(
-                      textColor: Colors.white,
-                      onPressed: () {
-                        if (_emailController.text == "ezz" &&
-                            _passwordController.text == "ezz") {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const BMIScreen()));
-                        } else {
-                          Toast.show("Invalid Email Address or Password",
-                              duration: Toast.lengthShort,
-                              gravity: Toast.bottom);
+                  getDefaultTextField(
+                      controller: _emailController,
+                      label: "Email Address",
+                      prefix: Icons.email,
+                      textInputType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Email cannot be empty";
                         }
-                      },
-                      child: const Text(
-                        "LOGIN",
+
+                        return null;
+                      }),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  getDefaultTextField(
+                      controller: _passwordController,
+                      textInputType: TextInputType.visiblePassword,
+                      prefix: Icons.lock,
+                      suffix: Icons.remove_red_eye,
+                      label: "Password",
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+
+                        return null;
+                      }),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(20)),
+                      color: Colors.blue,
+                    ),
+                    child: MaterialButton(
+                        textColor: Colors.white,
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            if (_emailController.text == "ezz" &&
+                                _passwordController.text == "ezz") {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const BMIScreen()));
+                            } else {
+                              Toast.show("Invalid Email Address or Password",
+                                  duration: Toast.lengthShort,
+                                  gravity: Toast.bottom);
+                            }
+                          }
+                        },
+                        child: const Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 21,
+                          ),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account ?",
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 21,
+                          fontSize: 14,
                         ),
-                      )),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Don't have an account ?",
-                      style: TextStyle(
-                        fontSize: 14,
                       ),
-                    ),
-                    TextButton(
-                      child: const Text(
-                        "Register Now",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                      TextButton(
+                        child: const Text(
+                          "Register Now",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () => Toast.show("Register Process",
+                            duration: Toast.lengthShort, gravity: Toast.bottom),
                       ),
-                      onPressed: () => print("Register......"),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-Text getTitleText(
+Widget getTitleText(
         {String text = "BMI Calculator",
         FontWeight fontWeight = FontWeight.bold,
         Color color = Colors.white}) =>
     Text(
       text,
       style: TextStyle(fontWeight: fontWeight, color: color),
+    );
+
+Widget getDefaultTextField(
+        {required TextEditingController controller,
+        required TextInputType textInputType,
+        required IconData prefix,
+        required String label,
+        bool isPassword = false,
+        Function? validator,
+        IconData? suffix}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: textInputType,
+      decoration: InputDecoration(
+        prefixIcon: Icon(prefix),
+        suffix: suffix != null ? Icon(suffix) : null,
+        labelText: (label),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Email cannot be empty";
+        }
+
+        return null;
+      },
     );
